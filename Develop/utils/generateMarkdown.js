5197977182c9a7634757f987872c429e2c43fc11
latoyadawson <78005063+latoyadawson@users.noreply.github.com>
2021-04-11 15:32:37 -0700

    cleanup

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -57,7 +57,6 @@ function renderLicenseBadge(license) {
       ![badge](https://img.shields.io/badge/license-${finalLis}-brightgreen)
     `;
 }
-//oo
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
